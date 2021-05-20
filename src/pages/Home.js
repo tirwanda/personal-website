6018b7d5da92ece7139b5270b8bd6fba8151d2b5
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-05-20 13:11:27 +0700

    Create Home Section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import heroImg from 'assets/images/hero-2-bw.png';
+import Button from 'elements/Button';
 
 export default function Home() {
 	return (
 		<div className="container">
-			<h5>
-				The <b>Tirwanda</b>, is a creative, engineer driven, global
-				agency working on advancing the software advertising and design
-				communities to new heights.
-			</h5>
+			<div className="hero">
+				<h1 className="hero-heading">
+					<span>Hello, This is</span>
+					<div className="hero-name">Edho Dwi Tirwanda</div>
+				</h1>
+				<div className="hero-img">
+					<img src={heroImg} alt="My Foto" />
+				</div>
+				<div className="hero-info">
+					<p>
+						I am working as full stack developer with 1+ years of
+						hands of experience designing, developing and
+						implementing application and solutions using a range of
+						technology and programming language.
+					</p>
+					<Button className="button" href="/portfolio" type="link">
+						See My Works
+					</Button>
+				</div>
+			</div>
 		</div>
 	);
 }
