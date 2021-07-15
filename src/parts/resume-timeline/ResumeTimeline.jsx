ed24e756435650f73a1ad5d76022880a8e0e837b
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-07-15 15:17:05 +0700

    Create part timeline

--- src/parts/resume-timeline/ResumeTimeline.jsx
+++ src/parts/resume-timeline/ResumeTimeline.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './timeline.scss';
 
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
 import Timeline from '@material-ui/lab/Timeline';
 import TimelineItem from '@material-ui/lab/TimelineItem';
 import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
@@ -9,12 +11,14 @@ import TimelineContent from '@material-ui/lab/TimelineContent';
 import TimelineDot from '@material-ui/lab/TimelineDot';
 import { Typography } from '@material-ui/core';
 
-const ResumeTimeline = ({ title }) => {
+const ResumeTimeline = ({ title, icon, children }) => {
 	return (
 		<Timeline className={'timeline'}>
 			<TimelineItem className={'timeline-first'}>
 				<TimelineSeparator>
-					<TimelineDot className={'timeline-dot'} />
+					<TimelineDot className={'timeline-dot-header'}>
+						{<AccountCircleIcon />}
+					</TimelineDot>
 					<TimelineConnector />
 				</TimelineSeparator>
 				<TimelineContent>
@@ -24,14 +28,22 @@ const ResumeTimeline = ({ title }) => {
 				</TimelineContent>
 			</TimelineItem>
 
-			<TimelineItem>
-				<TimelineSeparator>
-					<TimelineDot className={'timeline-dot'} />
-					<TimelineConnector />
-				</TimelineSeparator>
+			{children}
+
+			<TimelineItem className="timeline-content">
+				<CustomTimelineSeparator />
 				<TimelineContent>Code</TimelineContent>
 			</TimelineItem>
 		</Timeline>
+	);
+};
+
+export const CustomTimelineSeparator = () => {
+	return (
+		<TimelineSeparator className={'timeline-padding'}>
+			<TimelineDot variant={'outlined'} className={'timeline-dot'} />
+			<TimelineConnector />
+		</TimelineSeparator>
 	);
 };
 
