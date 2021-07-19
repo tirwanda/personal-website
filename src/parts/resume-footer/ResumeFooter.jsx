d442e9d8fbdeed31f255928821427d624b0e9061
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-07-19 23:15:36 +0700

    Create Resume Footer

--- src/parts/resume-footer/ResumeFooter.jsx
+++ src/parts/resume-footer/ResumeFooter.jsx
@@ -1,7 +1,23 @@
+import { Typography } from '@material-ui/core';
 import React from 'react';
+import './resumeFooter.scss';
 
 function ResumeFooter() {
-	return <div>This is a Footer for Resume</div>;
+	return (
+		<div className="footer">
+			<div className="footer-left">
+				<Typography className="name">Edho Dwi Tirwanda</Typography>
+			</div>
+			<div className="footer-right">
+				<Typography className="copyright">
+					Design and Develop by{' '}
+					<a href="/" target="_blank">
+						@tirwanda
+					</a>
+				</Typography>
+			</div>
+		</div>
+	);
 }
 
 export default ResumeFooter;
