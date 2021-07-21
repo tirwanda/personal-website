25c906597278eaf866909db6004ece3849591a53
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-07-21 22:20:29 +0700

    Creating Experience and Educational section

--- src/section/resume-cv/ResumeCv.jsx
+++ src/section/resume-cv/ResumeCv.jsx
@@ -2,7 +2,12 @@ import { Grid, Typography } from '@material-ui/core';
 import React from 'react';
 import './resumeCv.scss';
 
+import CustomeTimeline, {
+	CustomTimelineSeparator,
+} from 'parts/resume-timeline/ResumeTimeline';
 import data from 'util/resumeData';
+
+import { Work, School } from '@material-ui/icons';
 
 function Cv() {
 	return (
@@ -23,18 +28,27 @@ function Cv() {
 
 			{/* Education and Experience */}
 			<Grid container className="section">
-				<div className="section-left">
-					<Grid item className="section-title">
-						<span></span>
-						<h6 variant="h6">Working History</h6>
+				<Grid item className="section-title">
+					<span></span>
+					<h6 variant="h6">Resume</h6>
+				</Grid>
+
+				<Grid item xs={12}>
+					<Grid container>
+						<Grid item sm={12} md={6}>
+							<CustomeTimeline
+								title="Working Hitory"
+								icon={<Work />}
+							></CustomeTimeline>
+						</Grid>
+						<Grid item sm={12} md={6}>
+							<CustomeTimeline
+								title="Working Hitory"
+								icon={<School />}
+							></CustomeTimeline>
+						</Grid>
 					</Grid>
-				</div>
-				<div className="section-right">
-					<Grid item className="section-title">
-						<span></span>
-						<h6 variant="h6">Education History</h6>
-					</Grid>
-				</div>
+				</Grid>
 			</Grid>
 
 			{/* Service */}
