adbc861fcf50a35514082ed64948632639e2f94b
Edho Dwi Tirwanda <edhodwitirwanda@gmail.com>
2021-08-09 15:02:05 +0700

    Create Home Footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import './home.scss';
 
 import Hero from 'section/hero/Hero';
 import About from 'section/about/About';
+import Footer from 'parts/footer/Footer';
 
 export default function Home() {
 	return (
 		<>
 			<Hero />
 			<About />
+			<Footer />
 		</>
 	);
 }
---
+++ src/parts/footer/Footer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './footer.scss';
+
+import resumeData from 'util/resumeData';
+
+function Footer() {
+	return (
+		<div className="home-footer">
+			<div className="section-footer">
+				<p>
+					I makeas soon as possibale.You really like my work,if you
+					don&#8217;t i change this until you like I give you
+					seticfaction result
+				</p>
+				<div className="social-icons">
+					{Object.keys(resumeData.socials).map((key) => (
+						<a
+							key={key}
+							href={resumeData.socials[key].link}
+							target="_blank"
+							rel="noreferrer"
+						>
+							{resumeData.socials[key].icon}
+						</a>
+					))}
+				</div>
+				<p className="copyright">&copy;Copyright by Tirwanda</p>
+			</div>
+		</div>
+	);
+}
+
+export default Footer;
